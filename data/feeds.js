53892e62a7a724ca9d870640348dbc7cd159d147
Kristian Heruc <kristian@heruc.com>
2019-07-09 16:25:38 +0200

    Add a feeds data file

---
+++ data/feeds.js
@@ -0,0 +1,76 @@
+const feeds = [
+  {
+    title: 'Smashing Magazine',
+    slug: 'smashingmagazine',
+    website: 'https://www.smashingmagazine.com',
+    url: 'https://www.smashingmagazine.com/feed/'
+  },
+  {
+    title: 'CSS Tricks',
+    slug: 'css-tricks',
+    website: 'https://css-tricks.com',
+    url: 'http://feeds.feedburner.com/CssTricks'
+  },
+  {
+    title: 'freeCodeCamp',
+    slug: 'freecodecamp',
+    website: 'https://www.freecodecamp.org/news/',
+    url: 'https://www.freecodecamp.org/news/rss/'
+  },
+  {
+    title: 'DEV.to',
+    slug: 'dev',
+    website: 'https://dev.to',
+    url: 'https://dev.to/feed'
+  },
+  {
+    title: 'React.org',
+    slug: 'react',
+    website: 'https://reactjs.org',
+    url: 'https://reactjs.org/feed.xml'
+  },
+  {
+    title: 'Overreacted',
+    slug: 'overreacted',
+    website: 'https://overreacted.io',
+    url: 'https://overreacted.io/rss.xml'
+  },
+  {
+    title: 'JavaScript Playground',
+    slug: 'javascriptplayground',
+    website: 'https://javascriptplayground.com',
+    url: 'http://feeds.feedburner.com/TheJavascriptPlayground'
+  },
+  {
+    title: 'Scotch',
+    slug: 'scotch',
+    website: 'https://scotch.io',
+    url: 'https://scotch.io/feed'
+  },
+  {
+    title: 'Hacker Noon',
+    slug: 'hackernoon',
+    website: 'https://hackernoon.com',
+    url: 'https://hackernoon.com/feed'
+  },
+  {
+    title: 'Alligator.io',
+    slug: 'alligator',
+    website: 'https://alligator.io',
+    url: 'https://alligator.io/feed.xml'
+  },
+  {
+    title: 'Daily JS',
+    slug: 'dailyjs',
+    website: 'https://medium.com/dailyjs',
+    url: 'https://medium.com/feed/dailyjs'
+  },
+  {
+    title: 'Code Tuts+',
+    slug: 'tutsplus',
+    website: 'https://code.tutsplus.com',
+    url: 'https://code.tutsplus.com/posts.atom'
+  }
+];
+
+export default feeds;
