6061f1b7475208ac0a725abdd2b9472bbec06668
Kristian Heruc <kristian@heruc.com>
2019-07-09 17:04:09 +0200

    Fix file path

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App, { Container } from 'next/app';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 class MyApp extends App {
   render() {
