65c2b898d3c954df56fe48bea2d317f38b9cc597
CoderPrans <pranavbhaskar17@gmail.com>
2019-08-02 20:57:19 +0530

    no caching

--- next.config.js
+++ next.config.js
@@ -5,23 +5,23 @@ const nextConfig = {
   transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
   workboxOpts: {
     swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
+    // runtimeCaching: [
+    //   {
+    //     urlPattern: /^https?.*/,
+    //     handler: 'NetworkFirst',
+    //     options: {
+    //       cacheName: 'https-calls',
+    //       networkTimeoutSeconds: 15,
+    //       expiration: {
+    //         maxEntries: 150,
+    //         maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+    //       },
+    //       cacheableResponse: {
+    //         statuses: [0, 200],
+    //       },
+    //     },
+    //   },
+    // ],
   },
 };
 
